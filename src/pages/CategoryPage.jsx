500b85f07b2780e9e5fcf344910d44be921c2218
davidzheng14 <davidzheng144@gmail.com>
2026-05-07 23:55:32 -0400

    Move UI from ClosetPage to CategoryPage

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,10 +1,78 @@
 import { useParams } from "react-router";
+import { useState } from 'react';
 
-export function CategoryPage () {
+export function CategoryPage() {
+
+    const [display, setDisplay] = useState([]);
 
     const { category } = useParams();
+    
+    const [outfitMode, setOutfitMode] = useState(false);
+    const [outfitDraft, setOutfitDraft] = useState([]);
 
-    return(
-        <h1>{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
+    function addToDraft(item) {
+        { !outfitDraft.some(o => o.id === item.id) && setOutfitDraft([...outfitDraft, item]) }
+    }
+
+    function handleRemove(id) {
+        setCloset(prevCloset => prevCloset.filter((item) => item.id !== id));
+        setDisplay(prevDisplay => prevDisplay.filter((item) => item.id !== id));
+    }
+
+    function handleAddOutfit() {
+        setOutfitList([outfitDraft, ...outfitList]);
+        setOutfitDraft([]);
+    }
+    
+
+    return (
+        <>
+            <h1>{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
+
+
+            <div>
+                Sort by:
+                <select>
+                    <option> Latest </option>
+                    <option> Oldest </option>
+                    <option> Price [Low - High] </option>
+                    <option> Price [High - Low] </option>
+                </select>
+            </div>
+            <div>
+                <button onClick={handleMode}> Outfit Mode </button>: {outfitMode ? "On" : "Off"}
+            </div>
+            <div>
+                {display.map((item) => {
+                    return (
+                        <>
+                            <div className="item" key={item.id} onClick={outfitMode && (() => addToDraft(item))}>
+                                <div className="image">
+                                    <img src={item.image} width="100" height="100" />
+                                </div>
+                                <div> Brand: {item.brand} </div>
+                                <div> Type: {item.type} </div>
+                                <div> Size: {item.size} </div>
+                                <div> Color: {item.color} </div>
+                                <div> Price: {item.price} </div>
+                                <div> Date: {item.date} </div>
+
+                                <div> Category : {item.category} </div>
+                            </div>
+                            <button onClick={() => handleRemove(item.id)}> Remove </button>
+                        </>
+                    );
+                })}
+            </div>
+
+
+            <div>
+                <h1>Outfit Draft: </h1>
+                <ListViewer list={outfitDraft} />
+            </div>
+            <div>
+                {outfitDraft.length > 0 && <button onClick={handleAddOutfit}> Add Outfit </button>}
+            </div>
+        </>
     );
 }
--- src/pages/ClosetPage.jsx
+++ src/pages/ClosetPage.jsx
@@ -5,63 +5,52 @@ import './ClosetPage.css';
 
 export function ClosetPage({ closet, setCloset, outfitList, setOutfitList }) {
 
-    const [display, setDisplay] = useState([]);
-    const [outfitMode, setOutfitMode] = useState(false);
-    const [outfitDraft, setOutfitDraft] = useState([]);
+    
+    
 
-    function showAll() {
-        setDisplay([...closet]);
-    }
+    // function showAll() {
+    //     setDisplay([...closet]);
+    // }
 
-    function showTops() {
-        setDisplay([...closet].filter((item) => item.category == "tops"));
-    }
+    // function showTops() {
+    //     setDisplay([...closet].filter((item) => item.category == "tops"));
+    // }
 
-    function showBottoms() {
-        setDisplay([...closet].filter((item) => item.category == "bottoms"));
-    }
+    // function showBottoms() {
+    //     setDisplay([...closet].filter((item) => item.category == "bottoms"));
+    // }
 
-    function showOuterwear() {
-        setDisplay([...closet].filter((item) => item.category == "outerwear"));
-    }
+    // function showOuterwear() {
+    //     setDisplay([...closet].filter((item) => item.category == "outerwear"));
+    // }
 
-    function showOnePieces() {
-        setDisplay([...closet].filter((item) => item.category == "one-pieces"));
-    }
+    // function showOnePieces() {
+    //     setDisplay([...closet].filter((item) => item.category == "one-pieces"));
+    // }
 
-    function showUndergarments() {
-        setDisplay([...closet].filter((item) => item.category == "undergarments"));
-    }
+    // function showUndergarments() {
+    //     setDisplay([...closet].filter((item) => item.category == "undergarments"));
+    // }
 
-    function showFootwear() {
-        setDisplay([...closet].filter((item) => item.category == "footwear"));
-    }
+    // function showFootwear() {
+    //     setDisplay([...closet].filter((item) => item.category == "footwear"));
+    // }
 
-    function showAccessories() {
-        setDisplay([...closet].filter((item) => item.category == "accessories"));
-    }
+    // function showAccessories() {
+    //     setDisplay([...closet].filter((item) => item.category == "accessories"));
+    // }
 
-    function showOthers() {
-        setDisplay([...closet].filter((item) => item.category == "others"));
-    }
+    // function showOthers() {
+    //     setDisplay([...closet].filter((item) => item.category == "others"));
+    // }
 
-    function handleMode() {
-        setOutfitMode(outfitMode ? false : true);
-    }
+    // function handleMode() {
+    //     setOutfitMode(outfitMode ? false : true);
+    // }
 
-    function addToDraft(item) {
-        { !outfitDraft.some(o => o.id === item.id) && setOutfitDraft([...outfitDraft, item]) }
-    }
+    
 
-    function handleRemove(id) {
-        setCloset(prevCloset => prevCloset.filter((item) => item.id !== id));
-        setDisplay(prevDisplay => prevDisplay.filter((item) => item.id !== id));
-    }
-
-    function handleAddOutfit() {
-        setOutfitList([outfitDraft, ...outfitList]);
-        setOutfitDraft([]);
-    }
+    
 
 
 
@@ -127,49 +116,7 @@ export function ClosetPage({ closet, setCloset, outfitList, setOutfitList }) {
             </div>
             
 
-            <div>
-                Sort by:
-                <select>
-                    <option> Latest </option>
-                    <option> Oldest </option>
-                    <option> Price [Low - High] </option>
-                    <option> Price [High - Low] </option>
-                </select>
-            </div>
-            <div>
-                <button onClick={handleMode}> Outfit Mode </button>: {outfitMode ? "On" : "Off"}
-            </div>
-            <div>
-                {display.map((item) => {
-                    return (
-                        <>
-                            <div className="item" key={item.id} onClick={outfitMode && (() => addToDraft(item))}>
-                                <div className="image">
-                                    <img src={item.image} width="100" height="100" />
-                                </div>
-                                <div> Brand: {item.brand} </div>
-                                <div> Type: {item.type} </div>
-                                <div> Size: {item.size} </div>
-                                <div> Color: {item.color} </div>
-                                <div> Price: {item.price} </div>
-                                <div> Date: {item.date} </div>
-
-                                <div> Category : {item.category} </div>
-                            </div>
-                            <button onClick={() => handleRemove(item.id)}> Remove </button>
-                        </>
-                    );
-                })}
-            </div>
-
-
-            <div>
-                <h1>Outfit Draft: </h1>
-                <ListViewer list={outfitDraft} />
-            </div>
-            <div>
-                {outfitDraft.length > 0 && <button onClick={handleAddOutfit}> Add Outfit </button>}
-            </div>
+            
             
         </>
     );
