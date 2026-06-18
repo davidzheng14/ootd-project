a1d1a4d3b9a87e89e1fc324ced3c703290ff3b68
davidzheng14 <davidzheng144@gmail.com>
2026-06-17 22:45:21 -0400

    Created login variables

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxVnAzaa4jEpy5up5eHpmZN1GF_nVdm18",
+  authDomain: "ootd-abf1e.firebaseapp.com",
+  projectId: "ootd-abf1e",
+  storageBucket: "ootd-abf1e.firebasestorage.app",
+  messagingSenderId: "62227563488",
+  appId: "1:62227563488:web:2b5dd0aeb65b2ca9d0a185"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 export function Login() {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+
+
+
     return(
         <div>
             <input placeholder="Email"></input>
