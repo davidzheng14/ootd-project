cb0b254e646052f229b86db9131313df44afad1a
davidzheng14 <davidzheng144@gmail.com>
2026-06-18 00:13:38 -0400

    Removed key

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBxVnAzaa4jEpy5up5eHpmZN1GF_nVdm18",
-  authDomain: "ootd-abf1e.firebaseapp.com",
-  projectId: "ootd-abf1e",
-  storageBucket: "ootd-abf1e.firebasestorage.app",
-  messagingSenderId: "62227563488",
-  appId: "1:62227563488:web:2b5dd0aeb65b2ca9d0a185"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
