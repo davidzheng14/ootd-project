1ecd8b172c4d21cb888ede85432774f13e4aab36
davidzheng14 <davidzheng144@gmail.com>
2026-07-01 23:45:54 -0400

    Implemented authenication with firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,30 @@
 import { useState } from "react";
+import { app } from "../firebase.js";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export function Login() {
 
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
 
+    const auth = getAuth(app);
 
-
+    async function signUp() {
+        const user = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(user);
+    }
+    
+    async function login() {
+        const user = await signInWithEmailAndPassword(auth, email, password);
+        console.log(user);
+    }
 
     return(
         <div>
-            <input placeholder="Email"></input>
-            <input placeholder="Password"></input>
-            <div>Login</div>
+            <input onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
+            <input onChange={(e) => setPassword(e.target.value)} placeholder="Password"></input>
+            <button onClick={signUp}>Sign Up</button>
+            <button onClick={login}>Sign In</button>
         </div>
     );
 }
